2aadd10cdda1c67ae7f14fc6998270e4ee99f0f9
erpapa <erpapa@erpapadeMacBook-Pro.local>
2017-06-13 14:40:23 +0800

    Initial Commit

---
+++ AudioConverter/AudioConverter.h
@@ -0,0 +1,19 @@
+//
+//  AudioConverter.h
+//  AudioConverter
+//
+//  Created by erpapa on 2017/6/13.
+//  Copyright © 2017年 erpapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudioConverter.
+FOUNDATION_EXPORT double AudioConverterVersionNumber;
+
+//! Project version string for AudioConverter.
+FOUNDATION_EXPORT const unsigned char AudioConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioConverter/PublicHeader.h>
+
+
